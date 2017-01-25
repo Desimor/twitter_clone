f1414c3d828ef4a3e6165c1d0b7d340af889481f
Matthew Morais <moraismatthew@yahoo.com>
2017-01-24 19:56:40 -0500

    added html content to index.html to establish base layout for the home page of twitter_clone

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var server = express();
 var port = 8080;
 
 server.get('/', function(req, res){
-  res.sendfile('index.html', {root: __dirname + '/public/html'})
+  res.sendfile('index.html', {root: __dirname + '/public/html/'})
 });
 
 server.get('/login', function(req, res){
-  res.sendfile('login.html', {root: __dirname + '/public/html'})
+  res.sendfile('login.html', {root: __dirname + '/public/html/'})
 });
 
 server.get('/signup', function(req, res){
-  res.sendfile('signup.html', {root: __dirname + '/public/html'})
+  res.sendfile('signup.html', {root: __dirname + '/public/html/'})
 });
 
 server.listen(port, function(){
